2e1670df753fa29478237f7c85aa2ab10ccdf05b
Marttinen <roope.marttinen@cgi.com>
2023-05-09 17:20:15 +0300

    textfields and buttons

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -3,17 +3,18 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 
 import SideBar from "./SideBar";
-import { Container, Grid, Paper, Toolbar } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Container, Grid, Paper, TextField, Toolbar, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 
 const mdTheme = createTheme();
 
 export default function Dashboard() {
     const [items, setItems] = useState([]);
+    const [votingState, setVotingState] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
-            const items1 = [{ message: "Testi1" }]
+            const items1 = [{ name: "Test Candidate", votes: 5 }]
             setItems(items1);
         }
         if (true) {
@@ -41,11 +42,24 @@ return (
           }}
         >
             <Toolbar />
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Container maxWidth="lg" sx={{ mt: 14, mb: 4 }}>
                 <Grid container spacing={3}>
-                    {items && items.map((post: any) => (
-                        <Grid key={post.message} item xs={12} md={4} lg={3}>
-                        <Paper
+                    <TextField label='Candidate name'></TextField>
+                    <Button variant='contained' disabled={votingState}>Add</Button>
+                </Grid>
+            </Container>
+
+            <Container maxWidth="lg" sx={{ mt: 14, mb: 4 }}>
+                <Grid container spacing={3}>
+                    <Typography></Typography>
+                    <Button variant='contained' onClick={() => setVotingState(!votingState)}>{votingState ? "Stop voting" : "Start voting"}</Button>
+                </Grid>
+            </Container>
+            <Container maxWidth="lg" sx={{ mt: 10, mb: 4 }}>
+                <Grid container spacing={3}>
+                    {items && items.map((candidate: any) => (
+                        <Grid key={candidate.name} item xs={12} md={4} lg={3}>
+                        <Card
                             sx={{
                                 p: 2,
                                 display: 'flex',
@@ -53,8 +67,16 @@ return (
                                 height: 240,
                             }}
                         >
-                        {post.message}
-                        </Paper>
+                            <CardContent>
+                                <Typography variant='h5'>{candidate.name}</Typography>
+                                <Typography sx={{ mb: 1.5, mt: 1.5 }} color="text.secondary">Votes: {candidate.votes}</Typography>
+                                <br></br>
+                                <br></br>
+                            </CardContent>
+                            <CardActions>
+                                <Button disabled={!votingState}>Vote</Button>
+                            </CardActions>
+                        </Card>
                     </Grid>
                     ))}
                 </Grid>
